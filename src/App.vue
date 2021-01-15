c9b1a6cce943f4a0cd5811398c6428586b0ddc2f
David Matter <david.matter@me.com>
2021-01-15 21:06:34 +0100

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
   </div>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
+<script lang="ts">
+import Vue from 'vue';
+import HelloWorld from './components/HelloWorld.vue';
+import {StripeCardElement} from '@stripe/stripe-js'
 
-export default {
+const x: StripeCardElement | null = null
+console.log(x)
+
+export default Vue.extend({
   name: 'App',
   components: {
     HelloWorld
   }
-}
+});
 </script>
 
 <style>
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import {StripeCardElement} from '@stripe/stripe-js'
+
+const x: StripeCardElement | null = null
 Vue.config.productionTip = false
 
 new Vue({

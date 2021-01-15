6da1eeb6da166691463b33a722e6c4f23fbe6d19
David Matter <david.matter@me.com>
2021-01-15 21:08:55 +0100

    Setup demo

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,17 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <h1>Hi there :)</h1>
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
-import {StripeCardElement} from '@stripe/stripe-js'
+import { StripeCardElement } from '@stripe/stripe-js'
 
 const x: StripeCardElement | null = null
 console.log(x)
 
 export default Vue.extend({
   name: 'App',
-  components: {
-    HelloWorld
-  }
 });
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
+import { StripeCardElement } from '@stripe/stripe-js'
 
-import {StripeCardElement} from '@stripe/stripe-js'
 
 const x: StripeCardElement | null = null
 Vue.config.productionTip = false
